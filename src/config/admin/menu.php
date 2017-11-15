005c497bb1e8066c361bd6b776ddafcb46f3d8e6
semenov <semenov@skeeks.com>
2017-11-15 12:26:48 +0300

    update

--- src/config/admin/menu.php
+++ src/config/admin/menu.php
@@ -5,39 +5,36 @@
  * @copyright 2010 SkeekS (СкикС)
  * @date 15.04.2016
  */
-return
-    [
-        'marketplace' =>
+return [
+    'marketplace' => [
+        'priority' => 400,
+        'label' => \Yii::t('skeeks/marketplace', 'Marketplace'),
+        "img" => ['skeeks\cms\marketplace\assets\CmsMarkerplaceAsset', 'icons/marketplace.png'],
+
+        'items' => [
             [
-                'priority' => 400,
-                'label' => \Yii::t('skeeks/marketplace', 'Marketplace'),
-                "img" => ['skeeks\cms\marketplace\assets\CmsMarkerplaceAsset', 'icons/marketplace.png'],
-
-                'items' =>
-                    [
-                        [
-                            "label" => \Yii::t('skeeks/marketplace', "Catalog"),
-                            "url" => ["cmsMarketplace/admin-marketplace/catalog"],
-                            "img" => ['skeeks\cms\marketplace\assets\CmsMarkerplaceAsset', 'icons/marketplace.png']
-                        ],
-
-                        [
-                            "label" => \Yii::t('skeeks/marketplace', "Installed"),
-                            "url" => ["cmsMarketplace/admin-marketplace/index"],
-                            "img" => ['skeeks\cms\marketplace\assets\CmsMarkerplaceAsset', 'icons/installed.png']
-                        ],
-
-                        [
-                            "label" => \Yii::t('skeeks/marketplace', "Install{s}Delete", ['s' => '/']),
-                            "url" => ["cmsMarketplace/admin-marketplace/install"],
-                            "img" => ['skeeks\cms\marketplace\assets\CmsMarkerplaceAsset', 'icons/installer.png']
-                        ],
-
-                        [
-                            "label" => \Yii::t('skeeks/marketplace', "Updated platforms"),
-                            "url" => ["cmsMarketplace/admin-marketplace/update"],
-                            "img" => ['skeeks\cms\marketplace\assets\CmsMarkerplaceAsset', 'icons/update-2.png']
-                        ],
-                    ]
+                "label" => \Yii::t('skeeks/marketplace', "Catalog"),
+                "url" => ["cmsMarketplace/admin-marketplace/catalog"],
+                "img" => ['skeeks\cms\marketplace\assets\CmsMarkerplaceAsset', 'icons/marketplace.png']
             ],
-    ];
+
+            [
+                "label" => \Yii::t('skeeks/marketplace', "Installed"),
+                "url" => ["cmsMarketplace/admin-marketplace/index"],
+                "img" => ['skeeks\cms\marketplace\assets\CmsMarkerplaceAsset', 'icons/installed.png']
+            ],
+
+            [
+                "label" => \Yii::t('skeeks/marketplace', "Install{s}Delete", ['s' => '/']),
+                "url" => ["cmsMarketplace/admin-marketplace/install"],
+                "img" => ['skeeks\cms\marketplace\assets\CmsMarkerplaceAsset', 'icons/installer.png']
+            ],
+
+            [
+                "label" => \Yii::t('skeeks/marketplace', "Updated platforms"),
+                "url" => ["cmsMarketplace/admin-composer-update/update"],
+                "img" => ['skeeks\cms\marketplace\assets\CmsMarkerplaceAsset', 'icons/update-2.png']
+            ],
+        ]
+    ],
+];
--- src/config/web.php
+++ src/config/web.php
@@ -1,31 +1,27 @@
 <?php
 return [
-
-    'components' =>
-        [
-            'cmsMarketplace' =>
-                [
-                    'class' => '\skeeks\cms\marketplace\CmsMarketplaceComponent',
-                ],
-
-            'i18n' => [
-                'translations' =>
-                    [
-                        'skeeks/marketplace' => [
-                            'class' => 'yii\i18n\PhpMessageSource',
-                            'basePath' => '@skeeks/cms/marketplace/messages',
-                            'fileMap' => [
-                                'skeeks/marketplace' => 'main.php',
-                            ],
-                        ]
-                    ]
-            ],
+    'components' => [
+        'cmsMarketplace' => [
+            'class' => '\skeeks\cms\marketplace\CmsMarketplaceComponent',
         ],
 
-    'modules' =>
-        [
-            'cmsMarketplace' => [
-                'class' => 'skeeks\cms\marketplace\CmsMarketplaceModule',
-            ]
+        'i18n' => [
+            'translations' =>
+                [
+                    'skeeks/marketplace' => [
+                        'class' => 'yii\i18n\PhpMessageSource',
+                        'basePath' => '@skeeks/cms/marketplace/messages',
+                        'fileMap' => [
+                            'skeeks/marketplace' => 'main.php',
+                        ],
+                    ]
+                ]
+        ],
+    ],
+
+    'modules' => [
+        'cmsMarketplace' => [
+            'class' => 'skeeks\cms\marketplace\CmsMarketplaceModule',
         ]
+    ]
 ];
