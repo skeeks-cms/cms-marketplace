f3ec6c152f9750a34c531663a5da0140e569517f
Semenov <semenov@skeeks.com>
2019-11-18 13:55:02 +0300

    dev

--- src/config/web.php
+++ src/config/web.php
@@ -4,6 +4,44 @@ return [
         'cmsMarketplace' => [
             'class' => '\skeeks\cms\marketplace\CmsMarketplaceComponent',
         ],
+
+        'backendAdmin' => [
+            'menu' => [
+                'data' => [
+                    'marketplace' => [
+                        'priority' => 400,
+                        'name'    => ['skeeks/marketplace', 'Marketplace'],
+                        "image"      => ['skeeks\cms\marketplace\assets\CmsMarkerplaceAsset', 'icons/marketplace.png'],
+
+                        'items' => [
+                            [
+                                "name" => ['skeeks/marketplace', "Catalog"],
+                                "url"   => ["cmsMarketplace/admin-marketplace/catalog"],
+                                "image"   => ['skeeks\cms\marketplace\assets\CmsMarkerplaceAsset', 'icons/marketplace.png'],
+                            ],
+
+                            [
+                                "name" => ['skeeks/marketplace', "Installed"],
+                                "url"   => ["cmsMarketplace/admin-marketplace/index"],
+                                "image"   => ['skeeks\cms\marketplace\assets\CmsMarkerplaceAsset', 'icons/installed.png'],
+                            ],
+
+                            [
+                                "name" => ['skeeks/marketplace', "Install{s}Delete", ['s' => '/']],
+                                "url"   => ["cmsMarketplace/admin-marketplace/install"],
+                                "image"   => ['skeeks\cms\marketplace\assets\CmsMarkerplaceAsset', 'icons/installer.png'],
+                            ],
+
+                            [
+                                "name" => ['skeeks/marketplace', "Updated platforms"],
+                                "url"   => ["cmsMarketplace/admin-composer-update/update"],
+                                "image"   => ['skeeks\cms\marketplace\assets\CmsMarkerplaceAsset', 'icons/update-2.png'],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
     ],
 
     'modules' => [
